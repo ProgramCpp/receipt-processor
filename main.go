05ec6fc3ea89845315dff277a3811d3f8b934631
Janani Anbarasan <janani.anbarasan@gmail.com>
2023-10-31 20:27:00 -0500

    add create receipts endpoint

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/programcpp/receipt-processor/handlers"
 )
 
 func main(){
 	mux := http.NewServeMux()
+	
+	// init api handlers
+	mux.HandleFunc("/receipts/process", handlers.CreateReceipts)
 
+	log.Println("listening on port 1201...")
 	log.Fatal(http.ListenAndServe(":1201", mux))
 }
